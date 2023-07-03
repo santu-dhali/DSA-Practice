5c4dfb5ae5fa5ef019dd009f2877c4138860dee3
santu-dhali <96809296+santu-dhali@users.noreply.github.com>
2023-07-03 12:48:13 +0530

    Update (Easy)Set-Matrix-Zeroes.cpp

--- Arrays/(Easy)Set-Matrix-Zeroes.cpp
+++ Arrays/(Easy)Set-Matrix-Zeroes.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> ans = matrix;
+        int m = matrix.size();
+        int n = matrix[0].size();
 
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        ans[k][j] = 0;
+                    }
+                    for(int k=0; k<n; k++){
+                        ans[i][k] = 0;
+                    }
+                }
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                matrix[i][j] = ans[i][j];
+            }
+        }
+    }
+};
