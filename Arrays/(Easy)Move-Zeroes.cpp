5aa6eaaa2ae4b2913dfdf096783b60406923fc66
santu-dhali <96809296+santu-dhali@users.noreply.github.com>
2023-07-03 12:49:41 +0530

    Create (Easy)Move-Zeroes.cpp

---
+++ Arrays/(Easy)Move-Zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0){
+                ans.push_back(nums[i]);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            nums[i] = ans[i];
+        }
+    }
+};
