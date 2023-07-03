e0fcf5e6743b3b2e9ed8aaa2532f0c882ab71596
santu-dhali <96809296+santu-dhali@users.noreply.github.com>
2023-07-03 12:52:02 +0530

    Day -1

--- Arrays/(Easy)Set-Matrix-Zeroes.cpp
+++ Arrays/(Easy)Set-Matrix-Zeroes.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> ans = matrix;
         int m = matrix.size();
         int n = matrix[0].size();
-
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
