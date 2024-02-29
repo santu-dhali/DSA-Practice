47dc1077185a1f70345d186510e038b49c76c9b5
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-02-29 23:18:38 +0530

    Time: 194 ms (78.97%) | Memory: 149.2 MB (89.27%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+
+        queue<TreeNode*> queue;
+        int level = 0;
+
+        queue.push(root);
+
+        while (!queue.empty()) {
+            int size = queue.size();
+            int prev_val = (level % 2 == 0) ? numeric_limits<int>::min() : numeric_limits<int>::max();
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = queue.front();
+                queue.pop();
+
+                if ((level % 2 == 0 && (node->val % 2 == 0 || node->val <= prev_val)) ||
+                    (level % 2 == 1 && (node->val % 2 == 1 || node->val >= prev_val))) {
+                    return false;
+                }
+
+                prev_val = node->val;
+
+                if (node->left) {
+                    queue.push(node->left);
+                }
+                if (node->right) {
+                    queue.push(node->right);
+                }
+            }
+            level++;
+        }
+        return true;
+    }
+};
