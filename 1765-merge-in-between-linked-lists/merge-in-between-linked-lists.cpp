23c76baf3ac6429821e77a1559f394d23ff29252
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-03-21 12:45:47 +0530

    Time: 176 ms (68.38%) | Memory: 98.1 MB (13.45%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -1,19 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        ListNode* ptr = list1;
-        for (int i = 0; i < a - 1; ++i)
-            ptr = ptr->next;
-        
-        ListNode* qtr = ptr->next;
-        for (int i = 0; i < b - a + 1; ++i)
-            qtr = qtr->next;
-        
-        ptr->next = list2;
-        while (list2->next)
-            list2 = list2->next;
-        list2->next = qtr;
-        
+        // create a curr pointer for list1 and take it to a-1
+        // create another pointer and take it to b+1
+        ListNode* curr = list1;
+        ListNode* ptr1 = list1;
+        for(int i=0; i<=b; i++){
+            if(i<a-1){
+                curr = curr->next;
+            }
+            ptr1 = ptr1->next;
+        }
+
+        // create pointer for list2 and take it to the list node
+        ListNode* ptr2 = list2;
+        while(ptr2->next != NULL){
+            ptr2 = ptr2->next;
+        }
+
+        // now link curr with the list2 and ptr2 with the ptr1
+        curr->next = list2;
+        ptr2->next = ptr1;
+
         return list1;
     }
 };
