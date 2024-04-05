5f0786f7ff579e5990883f675e1c3abf458911dc
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-04-05 22:41:40 +0530

    Time: 632 ms (5.75%) | Memory: 346.5 MB (5.15%) - LeetSync

---
+++ 3292-earliest-second-to-mark-indices-i/earliest-second-to-mark-indices-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool solve(vector<int>& nums, vector<int>& changeIndices, int sec, int n, int m){
+        vector<int>lastInd(n+1, -1); // keeping track of last Index in changeIndices for each element of nums.
+
+        for(int i=0; i<=sec; i++){
+            int ind = changeIndices[i];
+            lastInd[ind] = i+1;
+        }
+
+        for(int i=1; i<=n; i++){
+            if(lastInd[i] == -1){
+                return false;    // checking if any index is left with -1 or not. If it's found then marking all indices is not possible
+            }
+        }
+
+        int marking_sec = 0;
+        map<int, int>mp; // keeping track of index and element of the vector lastInd in sorted order;
+        for(int i=1; i<=n; i++){
+            int position = lastInd[i];
+            mp[position] = nums[i-1];
+        }
+
+        for(auto it: mp){
+            if(it.second + 1 + marking_sec <= it.first){
+                marking_sec += it.second+1;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    int earliestSecondToMarkIndices(vector<int>& nums, vector<int>& changeIndices) {
+        int n = nums.size();
+        int m = changeIndices.size();
+
+        for(int i=0; i<m; i++){
+            if(solve(nums, changeIndices, i, n, m)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
