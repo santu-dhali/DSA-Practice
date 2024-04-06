8251367a2c7e74ff3857c68375b6c90d5ac09ad6
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-04-06 14:19:52 +0530

    Time: 19 ms (75.33%) | Memory: 12.2 MB (74.21%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int n = s.size();
+        int count = 0;
+        string ans="";
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '('){
+                count++;
+            }else if(s[i] == ')'){
+                if(count == 0){
+                    s[i]='*';
+                }else{
+                    count--;
+                }
+            }
+        }
+        for(int i=n-1; i>=0; i--){
+            if(count > 0 and s[i] == '('){
+                count--;
+                s[i]='*';
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(s[i] != '*'){
+                ans += s[i];
+            }
+        }
+
+        return ans;
+    }
+};
