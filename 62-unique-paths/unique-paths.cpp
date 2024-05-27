c6578e9ede1e12865d5cfb52722fc26efefe35ea
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-05-27 22:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (44.65%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
 int solve(int i, int j, vector<vector<int>>&dp){
-        if(i==0 or j==0){
+        if(i==0 and j==0){
             return 1;
         }
+        if(i<0 or j<0){
+            return 0;
+        }
+        
         if(dp[i][j] != -1){
             return dp[i][j];
         }
