a07667f9b572762334a6f0013ad0ec2d3fc710e9
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-06-11 22:22:59 +0530

    Time: 23 ms (20.62%) | Memory: 17 MB (62.54%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        int m = flights[0].size();
-        queue<pair<int, pair<int, int>>>pq;
         vector<pair<int, int>>adj[n];
-        
         for(auto it: flights){
             adj[it[0]].push_back({it[1], it[2]});
         }
+
+        queue<pair<int, pair<int, int>>>pq;
         vector<int>dis(n, 1e9);
-        pq.push({0, {src, 0}});
         dis[src] = 0;
-        
+        pq.push({0, {src, 0}});
+
         while(!pq.empty() and pq.front().second.second <= k){
             int dist = pq.front().first;
             int node = pq.front().second.first;
-            int rea = pq.front().second.second;
+            int val = pq.front().second.second;
             pq.pop();
-            
+
             for(auto it: adj[node]){
                 if(dist + it.second < dis[it.first]){
                     dis[it.first] = dist + it.second;
-                    pq.push({dis[it.first], {it.first, rea+1}});
+                    pq.push({dis[it.first], {it.first, val+1}});
                 }
             }
         }
@@ -29,5 +28,6 @@ public:
             return -1;
         }
         return dis[dst];
+
     }
 };
