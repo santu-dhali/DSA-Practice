7d4f7283b981a8828514abde019acbde869e0572
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-07-10 20:10:49 +0530

    Time: 4 ms (67.13%) | Memory: 13.8 MB (84.88%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans = 0;
+        for(auto it: logs){
+            if(it == "../"){
+                if(ans > 0){
+                    ans--;
+                }
+            }else if(it == "./"){
+                continue;
+            }else{
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
