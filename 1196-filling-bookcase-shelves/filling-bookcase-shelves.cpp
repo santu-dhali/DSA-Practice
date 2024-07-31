77e3753118f6e557d7a64ca084a3ee48765bcacb
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-07-31 23:46:01 +0530

    Time: 3 ms (87.31%) | Memory: 11.2 MB (46.57%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+    int solve(int ind, vector<vector<int>>&books, int shelfWidth, vector<int>&dp){
         int n = books.size();
-        int m = books[0].size();
-        vector<int>dp(n+1, 1e9);
-        dp[0]=0;
-
-        for(int i=1; i<=n; i++){
-            int width = 0;
-            int height = 0;
-            for(int j=i; j>0; j--){
-                width += books[j-1][0];
-                if(width > shelfWidth){
-                    break;
-                }
-                height = max(height, books[j-1][1]);
-                dp[i] = min(dp[i], dp[j-1]+height);
-            }
+        if(ind == n){
+            return 0;
         }
-        return dp[n];
+
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        int ans=1e9, height=0, width=0;
+
+        for(int i=ind; i<n; i++){
+            width += books[i][0];
+            if(width > shelfWidth){
+                break;
+            }
+            height = max(height, books[i][1]);
+            ans = min(ans, height + solve(i+1, books, shelfWidth, dp));
+        }
+        return dp[ind] = ans;
+
+    }
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        vector<int>dp(books.size(), -1);
+        return solve(0, books, shelfWidth, dp);
     }
 };
