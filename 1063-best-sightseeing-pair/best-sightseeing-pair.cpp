621cc1c88e3cb1352657d1574db99db4f653aa0b
Santu Dhali <96809296+santu-dhali@users.noreply.github.com>
2024-12-27 18:56:06 +0530

    Time: 3 ms (29.83%) | Memory: 45.5 MB (65.45%) - LeetSync

---
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int n = values.size();
+        int ans = 0, maxi = values[0];
+
+        for(int i=1; i<n; i++){
+            ans = max(ans, maxi + values[i]-i);
+            maxi = max(maxi, values[i]+i);
+        }
+
+        return ans;
+    }
+};
